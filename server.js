d997fbbd85aeaa015e7133beabff88aae2020269
ndm0501 <nadeem.endeavour@gmail.com>
2020-09-11 21:53:53 +0530

    fixing dynamic port

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ const data = require("./data");
 const cors = require("cors");
 const path = require("path");
 
-const PORT = process.env.PORT || 5000;
-
 // configure body parser for AJAX requests
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
@@ -19,7 +17,18 @@ app.get("/data", (req, res) => {
   res.send(data);
 });
 
-// Bootstrap server
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}.`);
+//serve static assests if in production
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server started at ${port}`);
 });
